885960f874f239bd542e36e2994a22be98baf486
Rohan Bendre <rohanbendre@Rohans-MacBook-Pro-2.local>
2015-11-09 10:08:58 -0500

    pattern for forgot password

---
+++ Forgot Password/ForgotPassword.js
@@ -0,0 +1,69 @@
+function ForgotPassword(req, resp){
+    
+    initClearBlade();
+    var user = ClearBlade.User();
+    var query = ClearBlade.Query();
+    query.equalTo('email',req.params.email);
+    user.allUsers(query, function(err,userArray) {
+        if(err) {
+            resp.success("Error");
+        }
+        else {
+            var resetToken = Math.floor((Math.random() * 1000000) + 1);
+            var d = new Date();
+            var collection = ClearBlade.Collection('collection_ID'); // reset collection has only admin perms
+            
+            var cb = function(err,result) {
+                if(err) {
+                    resp.error("Error!" + req);
+                }
+                else {
+                    var transport = mailer().createTransport({
+                    host:"smtp.gmail.com",
+                    port:"587"
+                    });
+                    transport.sendMail({
+                        from: "your_gmail_id",
+                        password:"your_password",
+                        to: "user_email",  // req.params.email
+                        subject: "Reset Token to change password",
+                        text: "Hi. Your reset token to change password is " + resetToken
+                    }, function(error, response){
+                        transport.close();
+                        if(error){
+                            resp.error(error);
+                        }else{
+                            resp.success("Reset token has been sent to " + req.params.email);
+                        }
+                    });
+                }
+            }
+            
+            var resetData = {
+                email:req.params.email,
+                reset_token:resetToken.toString(),
+                createdon: d.toISOString()
+            };
+            collection.create(resetData,cb);
+            // resp.success(resetToken);
+        }
+    });
+    
+    function initClearBlade() {
+        var initCallback = function(err, response) {
+            if (err) {
+                resp.error(response);
+            }
+        };
+        var initOptions = {
+            systemKey: req.systemKey,
+            systemSecret: req.systemSecret,
+            email: "your_email",
+            password: "your_password",
+            callback: initCallback
+        };
+        ClearBlade.init(initOptions);
+    }
+    
+    
+}
---
+++ Forgot Password/README.md
@@ -0,0 +1,12 @@
+# Pattern for generating reset token when user wants to change password
+
+- This service can be executed only by the user who has admin priviledges
+
+- Email of the user who wants to change his password should be given as parameter
+- A reset collection should be created that has admin rights
+- If the user exists in user table, reset token will be generated and put in reset collection
+- Reset token will be mailed to user which can be used to change password
+
+- The sample code is given in ForgotPassword.js
+- ClearBlade and Mailer need to be imported
+- Edit the credentials as indicated in sample code
