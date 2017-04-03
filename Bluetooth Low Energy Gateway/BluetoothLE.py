15ae94e06e5dd5527bc84a6ce6d616f03e9d02b0
Jim Bouquet <jbouquet@clearblade.com>
2017-04-03 11:45:22 -0500

    Modified code so that the adapter does not require as much user setup.

--- Bluetooth Low Energy Gateway/BluetoothLE.py
+++ Bluetooth Low Energy Gateway/BluetoothLE.py
@@ -1,42 +1,105 @@
 import thread
+import sys
 import time
+import json
+import string
 from bluepy.btle import Peripheral
 from bluepy.btle import Scanner
 from bluepy.btle import UUID
+from bluepy.btle import BTLEException
 
 class BluetoothLE:
-	def __init__(self, deviceAddress, deviceType, uuids, mqttConnection):
+	def __init__(self, deviceAddress, deviceType, addrType, mqttConnection):
 		self.deviceAddress = deviceAddress
 		self.deviceType = deviceType
-		self.uuids = uuids
 		self.mqtt = mqttConnection
-		self.bleConnection = self.ConnectToBLEDevice()
+		self.addrType = addrType
+		self.bleConnection = None
+		
+		self.ConnectToBLEDevice(True)
 
-	def ConnectToBLEDevice(self):
-		bleConnection = Peripheral(self.deviceAddress, "random")
-		print "Connected to " + self.deviceAddress
-		messageToPublish = "{\"deviceType\":\"" + self.deviceType + "\", \"deviceAddress\":\"" + self.deviceAddress + "\", \"status\": \"connected\"}"
-		self.mqtt.PublishMessage(messageToPublish, "BLEDeviceStatus")
+	def ConnectToBLEDevice(self, createNew):
+                print "Connecting to device ", self.deviceType
+                if createNew:
+                        self.bleConnection = Peripheral(self.deviceAddress, self.addrType)
+                        self.mqtt.setDeviceStatus(self, True)
+                else:
+                        self.bleConnection.connect(self.deviceAddress, self.addrType)
+                        
+		print "Connected to " + self.deviceType
 		thread.start_new_thread(self.ReadLoop, ())
-		return bleConnection
+
 
 	def ReadLoop(self):
-		while True:
-			for uuid in self.uuids:
-				try:
-					self.ReadValue(uuid)
-				except:
-					print "Failed to read data"
+                readData = {}
+		characteristics = self.bleConnection.getCharacteristics()
 
-			time.sleep(2)
+		for char in characteristics:
+                        if char.supportsRead():
+                                try:
+                                        value = char.read()
+                                        printable = True
+                                        #if the value is not printable, string escape it
+                                        for c in value:
+                                                if c in string.printable:
+                                                        printable = False
+                                                        break
+
+                                        if not printable:
+                                                value = value.encode('string-escape')
+
+                                        readData[str(char.uuid.getCommonName())] = value
+                                except:
+                                        print "Failed to read data for uuid ", str(char.uuid.getCommonName())
+                                        print sys.exc_info()
+
+                readData["services"] = []
+		
+		services = self.bleConnection.getServices()
+		for service in services:
+                        serviceData = {}
+                        serviceData["name"] = str(service.uuid.getCommonName())
+
+                        serviceChars = service.getCharacteristics()
+                        for serviceChar in serviceChars:
+                                if serviceChar.supportsRead():
+                                        try:
+                                                value = serviceChar.read()
+                                                printable = True
+                                                #if the value is not printable, string escape it
+                                                for c in value:
+                                                        if c in string.printable:
+                                                                printable = False
+                                                                break
+                                                if not printable:
+                                                        value = value.encode('string-escape')
+
+                                                serviceData[str(serviceChar.uuid.getCommonName())] = value
+                                        except:
+                                                print "Failed to read data for uuid ", str(serviceChar.uuid.getCommonName())
+                                                print sys.exc_info()
+
+                        if serviceData:
+                                readData["services"].append(serviceData)
+
+                print readData
+
+		message = {}
+		message["deviceAddress"] = self.deviceAddress
+		message["deviceType"] = self.deviceType
+		message["readData"] = str(readData)
+
+		self.mqtt.PublishMessage(json.dumps(message), "BLEReadData", None)
+
+		#Disconnect the peripheral once we are done reading data
+		self.bleConnection.disconnect()
 
 	def ReadValue(self, uuid):
-		uuidValue = UUID(uuid)
 		characteristics = self.bleConnection.getCharacteristics(uuid=uuid)[0]
 		readValue = characteristics.read()
-		print
+
 		messageToPublish = "{\"deviceType\":\"" + self.deviceType + "\", \"deviceAddress\":\"" + self.deviceAddress + "\", \"readData\": \"" + readValue + "\"}"
-		self.mqtt.PublishMessage(messageToPublish, "BLEReadData")
+		self.mqtt.PublishMessage(messageToPublish, "BLEReadData", None)
 
 	def WriteValue(self, data, uuid):
 		uuidValue = UUID(uuid)
--- Bluetooth Low Energy Gateway/MQTT.py
+++ Bluetooth Low Energy Gateway/MQTT.py
@@ -1,67 +1,32 @@
-import json
+import sys
 from clearblade import auth
 from clearblade import Client
 from clearblade import Messaging
-from BluetoothLE import BluetoothLE
 
 class MQTT:
-	connectedBLEDevices = {}
 	def __init__(self, credentials):
 		self.systemKey = credentials['systemKey']
 		self.systemSecret = credentials['systemSecret']
-		self.username = credentials['username']
-		self.password = credentials['password']
+		self.username = credentials['deviceName']
+		self.password = credentials['activeKey']
 		self.platformURL = credentials['platformURL']
-		self.gwBTMac = credentials['gwBTMac']
-		self.mqttConnection = self.ConnectToMQTT()
-		messageToPublish = "{\"gatewayAddress\":\"" + self.gwBTMac + "\", \"status\": \"Online\"}"
-		self.mqttConnection.publishMessage("ble/" + self.gwBTMac + "/BLEGatewayStatus", messageToPublish, 0)
-		self.Subscribe("ble/" + self.gwBTMac + "/BLECommands", self.MessageArrivedCallback)
-		while True:
-			pass
 
-	def ConnectToMQTT(self):
-		cbAuth = auth.Auth()
-		user = Client.UserClient(self.systemKey, self.systemSecret, self.username, self.password, self.platformURL)
-		cbAuth.Authenticate(user)
-		messagingClient = Messaging.Messaging(user)
-		messagingClient.InitializeMQTT()
+                #Connect to MQTT
+                self.messagingClient = self.Connect()
+
+	def Connect(self):
+                cbAuth = auth.Auth()
+
+                #Authenticate using device auth
+                device = Client.DevClient(self.systemKey, self.systemSecret, self.username, self.password, self.platformURL)
+                cbAuth.Authenticate(device)
+                messagingClient = Messaging.Messaging(device)
+
+                messagingClient.InitializeMQTT()
 		return messagingClient
 
-	def PublishMessage(self, message, messageType):
-		topic = "ble/" + self.gwBTMac + "/" + messageType
-		self.mqttConnection.publishMessage(topic, message, 0)
+	def PublishTopic(self, topic, message, callback):
+                self.messagingClient.publishMessage(topic, message, 0, callback)
 
-	def MessageArrivedCallback(self, client, obj, message):
-		arrivedMessageJSON = message.payload
-		print arrivedMessageJSON
-
-		try:
-			parsedMessage = json.loads(arrivedMessageJSON)
-			if parsedMessage['command'] == 'connect':
-				deviceAddress = parsedMessage['deviceAddress']
-				deviceType = parsedMessage['deviceType']
-				uuids = parsedMessage['uuids']
-				try:
-					bleConnection = BluetoothLE(deviceAddress, deviceType, uuids, self)
-					self.connectedBLEDevices[deviceAddress] = bleConnection
-				except:
-					print "Could not connect to " + deviceAddress
-					self.PublishMessage("Could not connect to " + deviceAddress, "BLEErrors")
-			elif parsedMessage['command'] == 'write':
-				writeDeviceAddress = parsedMessage['deviceAddress']
-				if self.connectedBLEDevices.has_key(writeDeviceAddress) == True:
-					try:
-						self.connectedBLEDevices[writeDeviceAddress].WriteValue(parsedMessage['data'], parsedMessage['uuid'])
-					except:
-						print "Error writing to BLE device"
-						self.PublishMessage("Error writing to BLE device " + deviceAddress, "BLEErrors")
-				else:
-					print writeDeviceAddress + ": No such device found in device dictionary. Please connect the device first and then try again"
-					self.PublishMessage(writeDeviceAddress + ": No such device found in device dictionary. Please connect the device first and then try again", "BLEErrors")
-		except:
-			print "Invalid JSON"
-			self.PublishMessage("Invalid JSON", "BLEErrors")
-
-	def Subscribe(self, topic, callback):
-		self.mqttConnection.subscribe(topic, 0, callback)
+	def SubscribeToTopic(self, topic, callback):
+		self.messagingClient.subscribe(topic, 0, callback)
